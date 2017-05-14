1c16dcf551dd0053b06fac0a77adfd993f1778de
Ivan Molodetskikh <yalterz@gmail.com>
2017-05-14 16:20:52 +0300

    Encapsulate all encoding stuff into an Encoder

--- src/encode.rs
+++ src/encode.rs
@@ -8,6 +8,119 @@ lazy_static! {
     static ref VIDEO_ENCODER: Mutex<Option<ffmpeg::codec::Video>> = Mutex::new(None);
 }
 
+struct Encoder {
+    converter: ffmpeg::software::scaling::Context,
+    context: ffmpeg::format::context::Output,
+    encoder: ffmpeg::codec::encoder::Video,
+    output_frame: ffmpeg::util::frame::Video,
+    packet: ffmpeg::Packet,
+
+    time_base: ffmpeg::Rational,
+    stream_time_base: ffmpeg::Rational,
+
+    pts: i64,
+}
+
+impl Encoder {
+    fn start(filename: &str,
+             codec: ffmpeg::codec::Video,
+             (width, height): (u32, u32),
+             time_base: ffmpeg::Rational) -> Result<Self> {
+        let mut context = ffmpeg::format::output(&filename)
+            .chain_err(|| "could not create the output context")?;
+
+        let encoder = {
+            let mut stream = context.add_stream(codec)
+                .chain_err(|| "could not add the video stream")?;
+
+            let mut encoder = stream.codec().encoder().video()
+                .chain_err(|| "could not retrieve the video encoder")?;
+
+            encoder.set_width(width);
+            encoder.set_height(height);
+            encoder.set_time_base(time_base);
+
+            if let Some(mut formats) = codec.formats() {
+                encoder.set_format(formats.next().unwrap());
+            } else {
+                encoder.set_format(ffmpeg::format::Pixel::YUV420P);
+            }
+
+            let encoder = encoder.open_as(codec)
+                .chain_err(|| "could not open the video encoder")?;
+            stream.set_parameters(&encoder);
+
+            stream.set_time_base(time_base);
+
+            encoder
+        };
+
+        context.write_header()
+            .chain_err(|| "could not write the header")?;
+
+        let stream_time_base = context.stream(0).unwrap().time_base();
+
+        let converter = ffmpeg::software::converter((width, height),
+                                                    ffmpeg::format::Pixel::RGB24,
+                                                    encoder.format())
+            .chain_err(|| "could not get the color conversion context")?;
+
+        let output_frame = ffmpeg::frame::Video::new(encoder.format(), width, height);
+
+        let packet = ffmpeg::Packet::empty();
+
+        Ok(Self {
+            converter,
+            context,
+            encoder,
+            output_frame,
+            packet,
+
+            time_base,
+            stream_time_base,
+
+            pts: 0,
+        })
+    }
+
+    fn encode(&mut self, frame: &ffmpeg::frame::Video) -> Result<()> {
+        self.converter.run(&frame, &mut self.output_frame)
+            .chain_err(|| "could not convert the frame to the correct format")?;
+
+        self.output_frame.set_pts(Some(self.pts));
+        self.pts += 1;
+
+        if self.encoder.encode(&self.output_frame, &mut self.packet)
+            .chain_err(|| "could not encode the frame")? {
+            self.packet.rescale_ts(self.time_base, self.stream_time_base);
+
+            self.packet.write_interleaved(&mut self.context)
+                .chain_err(|| "could not write the packet")?;
+        }
+
+        Ok(())
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        while self.encoder.flush(&mut self.packet)
+            .chain_err(|| "could not get the packet")? {
+            self.packet.rescale_ts(self.time_base, self.stream_time_base);
+
+            self.packet.write_interleaved(&mut self.context)
+                .chain_err(|| "could not write the packet")?;
+        }
+
+        Ok(())
+    }
+}
+
+impl Drop for Encoder {
+    fn drop(&mut self) {
+        self.flush();
+        self.context.write_trailer();
+    }
+}
+
 /// Initialize the encoding stuff. Should be called once.
 pub fn initialize() -> Result<()> {
     ffmpeg::init().chain_err(|| "error initializing ffmpeg")?;
@@ -79,82 +192,33 @@ fn test_video_output() -> Result<()> {
 
     let codec = codec.unwrap();
 
-    let mut context = ffmpeg::format::output(&"/home/yalter/test.mp4")
-        .chain_err(|| "could not create the output context")?;
+    let mut encoder = Encoder::start("/home/yalter/test.mkv",
+                                     codec,
+                                     (1280, 720),
+                                     (1, 60).into())
+        .chain_err(|| "could not create the encoder")?;
 
-    let mut encoder = {
-        let mut stream = context.add_stream(codec)
-            .chain_err(|| "could not add the video stream")?;
-
-        let mut encoder = stream.codec().encoder().video()
-            .chain_err(|| "could not retrieve the video encoder")?;
-
-        encoder.set_width(640);
-        encoder.set_height(360);
-        encoder.set_time_base((1, 60));
-
-        if let Some(mut formats) = codec.formats() {
-            encoder.set_format(formats.next().unwrap());
-        } else {
-            encoder.set_format(ffmpeg::format::Pixel::YUV420P);
-        }
-
-        let encoder = encoder.open_as(codec).chain_err(|| "could not open the video encoder")?;
-        stream.set_parameters(&encoder);
-
-        stream.set_time_base((1, 60));
-
-        encoder
-    };
-
-    context.write_header()
-        .chain_err(|| "could not write the header")?;
-
-    let stream_time_base = context.stream(0).unwrap().time_base();
-
-    let mut converter = ffmpeg::software::converter((640, 360),
-                                                ffmpeg::format::Pixel::RGB24,
-                                                encoder.format())
-        .chain_err(|| "could not get the color conversion context")?;
+    let mut frame = ffmpeg::frame::Video::new(ffmpeg::format::Pixel::RGB24, 1280, 720);
 
     {
-        let mut frame = ffmpeg::frame::Video::new(ffmpeg::format::Pixel::RGB24, 640, 360);
-        let mut output_frame = ffmpeg::frame::Video::new(encoder.format(), 640, 360);
-
-        for i in 0..255 {
-            {
-                let mut y = frame.plane_mut::<(u8, u8, u8)>(0);
-
-                for j in 0..y.len() {
-                    y[j] = (255, i as u8, 255 - i as u8);
-                }
-            }
-
-            converter.run(&frame, &mut output_frame)
-                .chain_err(|| "could not convert the frame to the correct format")?;
-
-            output_frame.set_pts(Some(i));
-
-            let mut packet = ffmpeg::Packet::empty();
-            if encoder.encode(&output_frame, &mut packet).chain_err(|| "could not encode the frame")? {
-                packet.rescale_ts((1, 60), stream_time_base);
-
-                packet.write_interleaved(&mut context)
-                    .chain_err(|| "could not write the packet")?;
-            }
-        }
-
-        let mut packet = ffmpeg::Packet::empty();
-        while encoder.flush(&mut packet).chain_err(|| "could not get the packet")? {
-            packet.rescale_ts((1, 60), stream_time_base);
-
-            packet.write_interleaved(&mut context)
-                .chain_err(|| "could not write the packet")?;
+        let mut data = frame.plane_mut::<(u8, u8, u8)>(0);
+        for i in 0..data.len() {
+            data[i] = (0, 255, 0);
         }
     }
 
-    context.write_trailer()
-        .chain_err(|| "could not write the trailer")?;
+    for f in 0..240 {
+        {
+            let mut data = frame.plane_mut::<(u8, u8, u8)>(0);
+            for y in 0..720 {
+                data[y * 1280 + f * 2] = (255, 0, 0);
+                data[y * 1280 + f * 2 + 1] = (255, 0, 0);
+            }
+        }
+
+        encoder.encode(&frame)
+            .chain_err(|| "could not encode a frame")?;
+    }
 
     Ok(())
 }
