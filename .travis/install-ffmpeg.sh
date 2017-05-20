01a5ecef8b033562803bf1e36061a6001a8f2770
Ivan Molodetskikh <yalterz@gmail.com>
2017-05-20 13:40:10 +0300

    Reduce the number of ffmpeg building threads

--- .travis/install-ffmpeg.sh
+++ .travis/install-ffmpeg.sh
@@ -6,7 +6,7 @@ if [ ! -d "$HOME/ffmpeg/lib" ]; then
 	mkdir "$HOME/ffmpeg_build"
 	cd "$HOME/ffmpeg_build"
 	../ffmpeg_src/configure --disable-programs --disable-doc --enable-cross-compile --arch=x86_32 --target_os=linux --prefix="$HOME/ffmpeg" --cc="gcc -m32" --disable-static --enable-shared
-	make -j && make install
+	make -j2 && make install
 	cd ..
 	rm -rf ffmpeg_src ffmpeg_build
 else
