97c0def6fbe1313652ddc5bb597282a1f3f11c36
Ivan Molodetskikh <yalterz@gmail.com>
2017-05-20 12:01:58 +0300

    Build and cache ffmpeg

---
+++ .travis/install-ffmpeg.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -ex
+
+if [ ! -d "$HOME/ffmpeg" ]; then
+	git clone https://github.com/FFMpeg/FFMpeg.git "$HOME/ffmpeg_src"
+	mkdir "$HOME/ffmpeg_build"
+	cd "$HOME/ffmpeg_build"
+	../ffmpeg_src/configure --disable-programs --disable-doc --enable-cross-compile --arch=x86_32 --target_os=linux --prefix="$HOME/ffmpeg" --cc="gcc -m32" --disable-static --enable-shared
+	make && make install
+	cd ..
+	rm -rf ffmpeg_src ffmpeg_build
+else
+	echo "Using cached directory."
+fi
