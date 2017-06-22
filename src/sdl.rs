4d52e773e8d69ad906689b11bc1829a18cb3ea7c
Ivan Molodetskikh <yalterz@gmail.com>
2017-06-22 22:50:54 +0300

    Fix undefined behaviour in sdl::get_proc_address()

--- src/sdl.rs
+++ src/sdl.rs
@@ -3,11 +3,8 @@ use sdl2_sys;
 use std::ffi::CString;
 
 pub fn get_proc_address(name: &str) -> *const c_void {
-    unsafe {
-        sdl2_sys::SDL_GL_GetProcAddress(CString::new(name)
-                                            .expect("could not convert name to a CString")
-                                            .as_ptr())
-    }
+    let cstring = CString::new(name).expect("could not convert name to a CString");
+    unsafe { sdl2_sys::SDL_GL_GetProcAddress(cstring.as_ptr()) }
 }
 
 pub fn get_current_context() -> u32 {
