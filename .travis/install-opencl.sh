68e15efdfb3b65a25bd46ca96c39138ec25db125
Ivan Molodetskikh <yalterz@gmail.com>
2019-06-02 19:08:45 +0300

    Travis: update OpenCL link

--- .travis/install-opencl.sh
+++ .travis/install-opencl.sh
@@ -4,8 +4,8 @@ set -ex
 if [ ! -d "$HOME/OpenCL/lib" ]; then
 	mkdir "OpenCL"
 	cd "OpenCL"
-	wget "https://archive.archlinux.org/packages/l/lib32-ocl-icd/lib32-ocl-icd-2.2.11-1-x86_64.pkg.tar.xz"
-	tar xJf "lib32-ocl-icd-2.2.11-1-x86_64.pkg.tar.xz"
+	wget "https://archive.archlinux.org/packages/l/lib32-ocl-icd/lib32-ocl-icd-2.2.12-2-x86_64.pkg.tar.xz"
+	tar xJf "lib32-ocl-icd-2.2.12-2-x86_64.pkg.tar.xz"
 	mkdir -p "$HOME/OpenCL/lib"
 	mv usr/lib32/* "$HOME/OpenCL/lib/"
 else
